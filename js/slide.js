79d1a8a9fc129a40f7a22570ff71636a85df5113
wjdgus1122 <106130435+wjdgus1122@users.noreply.github.com>
2022-06-19 21:46:25 +0900

    slide_Ex

--- js/slide.js
+++ js/slide.js
@@ -1,7 +1,31 @@
 $(document).ready(function () {
-  $(".right").click(function () {
+  var i = 0;
+  setInterval(function () {
+    i++;
+    if (i > 2) {
+      i = 0;
+    }
     $(".img_wrap").animate({
-      left: -1000,
+      left: -1000 * i,
+    });
+  }, 1000);
+  $(".right").click(function () {
+    i++;
+    if (i > 2) {
+      i = 0;
+    }
+    $(".img_wrap").animate({
+      left: -1000 * i,
+    });
+  });
+
+  $(".left").click(function () {
+    i--;
+    if (i < 0) {
+      i = 2;
+    }
+    $(".img_wrap").animate({
+      left: -1000 * i,
     });
   });
 });
